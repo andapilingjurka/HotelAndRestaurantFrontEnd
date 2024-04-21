71c12e17b5d5242329af3a16daa2fd505acffd3c
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-04-21 02:38:52 +0200

    Instalimi i Paketes per Bootstrap dhe icons

--- hotelandrestaurant/src/App.js
+++ hotelandrestaurant/src/App.js
@@ -1,5 +1,7 @@
 import FoodsCrud from './Admin/FoodsCrud';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 function App() {
