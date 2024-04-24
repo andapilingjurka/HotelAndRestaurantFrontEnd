ae5afc6e67ec2de889d829f2425610529cd8fb88
anda <ap56170@ubt-uni.net>
2024-04-23 22:38:02 -0700

    Instalimi i Paketave tjera

--- hotelandrestaurant/src/App.js
+++ hotelandrestaurant/src/App.js
@@ -1,22 +1,17 @@
-import FoodsCrud from './Admin/FoodsCrud';
+import FoodsCrud from "./Admin/FoodsCrud";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MenuCrud from './Admin/MenuCrud';
-import Menu from './components/Menu/Menu';
-
-
+import MenuCrud from "./Admin/MenuCrud";
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
     <Router>
-        <Routes>
-        <Route path="/" element={<Menu />} />
-          <Route path="/foods" element={<FoodsCrud />} />
-          <Route path="/menu" element={<MenuCrud />} />
-          
-
-        
-        </Routes>
+      <Routes>
+        <Route path="/usermenu" element={<Menu />} />
+        <Route path="/foods" element={<FoodsCrud />} />
+        <Route path="/menu" element={<MenuCrud />} />
+      </Routes>
     </Router>
   );
 }
